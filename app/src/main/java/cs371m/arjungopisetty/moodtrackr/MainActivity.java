becfc1f67b0d9d1cefebd670712293736b670fb8
arjungopisetty <arjun.gopisetty@gmail.com>
2018-03-27 21:38:15 -0500

    Tone analyzer working, returns as JSON

--- app/src/main/java/cs371m/arjungopisetty/moodtrackr/MainActivity.java
+++ app/src/main/java/cs371m/arjungopisetty/moodtrackr/MainActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private EditText mInputText;
+    private Button mAnalyzeButton;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -20,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    // TODO: Home fragment
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    // TODO: Graph fragment
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    // TODO: Insights fragment
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -41,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mInputText = (EditText) findViewById(R.id.inputText);
+        mAnalyzeButton = (Button) findViewById(R.id.analyzeButton);
+        mAnalyzeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WatsonAnalyzer analyzer = new WatsonAnalyzer();
+                String input = mInputText.getText().toString();
+                Log.d(TAG, "Tone input: " + input);
+                analyzer.analyzeText(input);
+            }
+        });
     }
 
 }
