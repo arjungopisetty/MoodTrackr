930c011df1910509a0dd6f0d2e5e4d714e52c0d5
arjungopisetty <arjun.gopisetty@gmail.com>
2018-03-30 21:42:30 -0500

    Adding in visualization graph fragment

---
+++ app/src/main/java/cs371m/arjungopisetty/moodtrackr/GraphFragment.java
@@ -0,0 +1,40 @@
+package cs371m.arjungopisetty.moodtrackr;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GraphFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GraphFragment extends Fragment {
+
+    public GraphFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static GraphFragment newInstance() {
+        GraphFragment fragment = new GraphFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_graph, container, false);
+    }
+
+}
